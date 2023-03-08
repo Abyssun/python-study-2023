43b17cc85b4d7fa524bb4cf232e3c46c039429a8
Abyssun <2025353414@qq.com>
2023-03-08 10:42:52 +0800

    Update test1.py

--- week2/课堂练习/test1.py
+++ week2/课堂练习/test1.py
@@ -1,5 +1,15 @@
+# import turtle
+# turtle.setup(800,800)
+# turtle.pensize(10)
+# turtle.pencolor("red")
+# turtle.speed(10)
+# turtle.circle(-10)
+# turtle.fd(50)
+# turtle.circle(100)
+
 import turtle
-turtle.setup(800,800)
+turtle.speed(1)
+turtle.setup(1000,1000)
 turtle.pensize(10)
 turtle.pencolor("red")
-turtle.circle(-100)
+turtle.circle(100,-90)
